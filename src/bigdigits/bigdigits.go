93509734bfc8f2b6f4dd212ee6355e9844959818
Hamish MacDonald <hamish.macdonald@servian.com>
2021-06-27 18:11:30 +1000

    modified code for exercise 1.8

--- src/bigdigits/bigdigits.go
+++ src/bigdigits/bigdigits.go
@@ -1,19 +1,43 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
+const usageMessage = "usage: bigdigits [-b|--bar] <whole-number>\n" +
+	"-b --bar draw an underbar and an overbar"
+var helpFlag = flag.Bool("help", false, usageMessage)
+var barFlag = flag.Bool("bar", false, "-b --bar draw an underbar and an overbar")
+
+func init() {
+	flag.BoolVar(helpFlag, "h", false, usageMessage)
+	flag.BoolVar(barFlag, "b", false, "-b --bar draw an underbar and an overbar")
+}
+
 func main() {
+
+	flag.Parse()
+	if *helpFlag {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 	if len(os.Args) == 1 {
 		fmt.Printf("usage: %s <whole-number>\n", filepath.Base(os.Args[0]))
 		os.Exit(1)
 	}
 
-	stringOfDigits := os.Args[1]
+	stringOfDigits := ""
+	if *barFlag {
+		stringOfDigits = os.Args[2]
+	} else {
+		stringOfDigits = os.Args[1]
+	}
+	lenLine := 0
 	for row := range bigDigits[0] {
 		line := ""
 		for column := range stringOfDigits {
@@ -24,7 +48,14 @@ func main() {
 				log.Fatal("invalid whole number")
 			}
 		}
+		lenLine = len(line)
+		if row == 0 && *barFlag {
+			fmt.Println(strings.Repeat("*", lenLine - 1))
+		}
 		fmt.Println(line)
+		}
+	if *barFlag {
+		fmt.Println(strings.Repeat("*", lenLine-1))
 	}
 }
 
@@ -33,8 +64,7 @@ var bigDigits = [][]string{
 	{" 1 ", "11 ", " 1 ", " 1 ", " 1 ", " 1 ", "111"},
 	{" 222 ", "2   2", "   2 ", "  2  ", " 2   ", "2    ", "22222"},
 	{" 333 ", "3   3", "    3", "  33 ", "    3", "3   3", " 333 "},
-	{"   4  ", "  44  ", " 4 4  ", "4  4  ", "444444", "   4  ",
-		"   4  "},
+	{"   4  ", "  44  ", " 4 4  ", "4  4  ", "444444", "   4  ",  "   4  "},
 	{"55555", "5    ", "5    ", " 555 ", "    5", "5   5", " 555 "},
 	{" 666 ", "6    ", "6    ", "6666 ", "6   6", "6   6", " 666 "},
 	{"77777", "    7", "   7 ", "  7  ", " 7   ", "7    ", "7    "},
